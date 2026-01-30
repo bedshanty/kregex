a8ea5221c1985d251a76601320f717c021ae9910
bedshanty <bedshanty@gmail.com>
2026-01-31 05:49:03 +0900

    build: configure maven-publish and signing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     kotlin("jvm") version "2.1.21"
+    `maven-publish`
+    signing
 }
 
 group = "io.github.bedshanty"
@@ -16,6 +18,82 @@ dependencies {
 tasks.test {
     useJUnitPlatform()
 }
+
 kotlin {
     jvmToolchain(17)
+}
+
+val sourcesJar by tasks.registering(Jar::class) {
+    archiveClassifier.set("sources")
+    from(sourceSets.main.get().allSource)
+}
+
+val javadocJar by tasks.registering(Jar::class) {
+    archiveClassifier.set("javadoc")
+    from(tasks.named("javadoc"))
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+            artifact(sourcesJar)
+            artifact(javadocJar)
+
+            pom {
+                name.set("kregex")
+                description.set("A Kotlin DSL library for building regular expressions easily")
+                url.set("https://github.com/bedshanty/kregex")
+
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+
+                developers {
+                    developer {
+                        id.set("bedshanty")
+                        name.set("Ji Heon Lee")
+                        email.set("bedshanty@gmail.com")
+                    }
+                }
+
+                scm {
+                    connection.set("scm:git:git://github.com/bedshanty/kregex.git")
+                    developerConnection.set("scm:git:ssh://github.com/bedshanty/kregex.git")
+                    url.set("https://github.com/bedshanty/kregex")
+                }
+            }
+        }
+    }
+
+    repositories {
+        maven {
+            name = "OSSRH"
+            val releasesRepoUrl = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+            val snapshotsRepoUrl = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+            url = if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
+
+            credentials {
+                username = project.findProperty("ossrhUsername") as String? ?: System.getenv("OSSRH_USERNAME")
+                password = project.findProperty("ossrhPassword") as String? ?: System.getenv("OSSRH_PASSWORD")
+            }
+        }
+    }
+}
+
+signing {
+    val signingKey = project.findProperty("signing.key") as String? ?: System.getenv("SIGNING_KEY")
+    val signingPassword = project.findProperty("signing.password") as String? ?: System.getenv("SIGNING_PASSWORD")
+
+    if (signingKey != null && signingPassword != null) {
+        useInMemoryPgpKeys(signingKey, signingPassword)
+        sign(publishing.publications["mavenJava"])
+    }
+}
+
+tasks.withType<Sign>().configureEach {
+    onlyIf { project.hasProperty("signing.key") || System.getenv("SIGNING_KEY") != null }
 }
