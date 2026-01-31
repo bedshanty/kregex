cb59059e89e462bd4dad0cec0dd317773f3809e6
bedshanty <bedshanty@gmail.com>
2026-02-01 03:39:52 +0900

    build: migrate to vanniktech maven publish plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,9 @@
+import com.vanniktech.maven.publish.SonatypeHost
+
 plugins {
     kotlin("jvm") version "2.1.21"
-    `maven-publish`
-    signing
     id("org.jetbrains.dokka") version "1.9.20"
+    id("com.vanniktech.maven.publish") version "0.30.0"
 }
 
 group = "io.github.bedshanty"
@@ -24,78 +25,35 @@ kotlin {
     jvmToolchain(17)
 }
 
-val sourcesJar by tasks.registering(Jar::class) {
-    archiveClassifier.set("sources")
-    from(sourceSets.main.get().allSource)
-}
+mavenPublishing {
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
 
-val javadocJar by tasks.registering(Jar::class) {
-    archiveClassifier.set("javadoc")
-    dependsOn(tasks.dokkaHtml)
-    from(tasks.dokkaHtml.flatMap { it.outputDirectory })
-}
+    signAllPublications()
 
-publishing {
-    publications {
-        create<MavenPublication>("mavenJava") {
-            from(components["java"])
-            artifact(sourcesJar)
-            artifact(javadocJar)
+    pom {
+        name.set("kregex")
+        description.set("A Kotlin DSL library for building Regex easily")
+        url.set("https://github.com/bedshanty/kregex")
 
-            pom {
-                name.set("kregex")
-                description.set("A Kotlin DSL library for building regular expressions easily")
-                url.set("https://github.com/bedshanty/kregex")
-
-                licenses {
-                    license {
-                        name.set("The Apache License, Version 2.0")
-                        url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
-                    }
-                }
-
-                developers {
-                    developer {
-                        id.set("bedshanty")
-                        name.set("Ji Heon Lee")
-                        email.set("bedshanty@gmail.com")
-                    }
-                }
-
-                scm {
-                    connection.set("scm:git:git://github.com/bedshanty/kregex.git")
-                    developerConnection.set("scm:git:ssh://github.com/bedshanty/kregex.git")
-                    url.set("https://github.com/bedshanty/kregex")
-                }
+        licenses {
+            license {
+                name.set("The Apache License, Version 2.0")
+                url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
             }
         }
-    }
 
-    repositories {
-        maven {
-            name = "OSSRH"
-            val releasesRepoUrl = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
-            val snapshotsRepoUrl = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
-            url = if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
-
-            credentials {
-                username = project.findProperty("ossrhUsername") as String? ?: System.getenv("OSSRH_USERNAME")
-                password = project.findProperty("ossrhPassword") as String? ?: System.getenv("OSSRH_PASSWORD")
+        developers {
+            developer {
+                id.set("bedshanty")
+                name.set("Ji Heon Lee")
+                email.set("bedshanty@gmail.com")
             }
         }
+
+        scm {
+            connection.set("scm:git:git://github.com/bedshanty/kregex.git")
+            developerConnection.set("scm:git:ssh://github.com/bedshanty/kregex.git")
+            url.set("https://github.com/bedshanty/kregex")
+        }
     }
-}
-
-signing {
-    val signingKey = project.findProperty("signing.key") as String? ?: System.getenv("SIGNING_KEY")
-    val signingPassword = project.findProperty("signing.password") as String? ?: System.getenv("SIGNING_PASSWORD")
-
-    if (signingKey != null && signingPassword != null) {
-        useInMemoryPgpKeys(signingKey, signingPassword)
-        sign(publishing.publications["mavenJava"])
-    }
-}
-
-tasks.withType<Sign>().configureEach {
-    onlyIf { project.hasProperty("signing.key") || System.getenv("SIGNING_KEY") != null }
 }
